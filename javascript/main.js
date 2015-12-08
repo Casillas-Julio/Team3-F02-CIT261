fc68be6eeb4b935d64ebc3950871e5269fadd6d3
ashliehorst <ashliehorst@yahoo.com>
2015-12-08 03:07:39 -0700

    Function added to main.js. Profile content in profile-template.html displays via AJAX. Nutrition and profile info can now be toggled.

--- javascript/main.js
+++ javascript/main.js
@@ -106,3 +106,14 @@ function loadData(codeDay, obj) {
     }
     //}
 }
+
+function loadContent(page){
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            document.getElementById("article").innerHTML = xmlhttp.responseText;
+        }
+    }
+    xmlhttp.open("GET", page, false);
+    xmlhttp.send();
+}
